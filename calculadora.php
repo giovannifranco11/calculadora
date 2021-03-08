2c7fdedcb0ad9f8654c78eafebb05bee5046bec6
giovannifranco <cgfranco72813@umanizales.edu.co>
2021-03-07 22:27:57 -0500

    Primer ejercicio en clase. Cliente-servidor con calculadora

---
+++ calculadora.php
@@ -0,0 +1,12 @@
+Página 1 PHP<br/>
+Hola <b>calculadora</b>
+
+<?php
+    $a = $_REQUEST['n1'];
+    $b = $_REQUEST['n2'];
+    $suma = $a + $b;
+?>
+<br/>
+La suma es <?php echo $suma; ?>
+<br/>
+El total es <?php echo $suma; ?>
